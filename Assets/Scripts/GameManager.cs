8ed4ff06c0b1f2da8cc345db81ba64004ddb4add
Tim-Niclas Oelschläger <zokki.softwareschmiede@gmail.com>
2025-01-18 17:46:05 +0100

    Display Touch and Simon

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Assets.Scripts.Simons;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -10,6 +7,8 @@ namespace Assets.Scripts
     internal class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
+
+        public bool IsSequencePlaying { get; private set; }
 
         public void Start()
         {
@@ -20,6 +19,15 @@ namespace Assets.Scripts
         internal void GameOver()
         {
             throw new NotImplementedException();
+        }
+
+        public void StartSequence()
+        {
+            if (IsSequencePlaying) return;
+
+            SimonsManager simonsManager = new SimonsManager();
+            SimonSequence sequence = simonsManager.GenerateSequence(5, 3);
+            StartCoroutine(simonsManager.PlaySequenceCoroutine(sequence));
         }
     }
 }
--- Assets/Scripts/Simons/SimonsManager.cs
+++ Assets/Scripts/Simons/SimonsManager.cs
@@ -1,8 +1,9 @@
 using Assets.Scripts.Simons;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimonsManager : MonoBehaviour
+public class SimonsManager
 {
     public SimonSequence GenerateSequence(int length, int inputDuration)
     {
@@ -22,5 +23,32 @@ public class SimonsManager : MonoBehaviour
         }
 
         return sequence;
+    }
+
+    public IEnumerator PlaySequenceCoroutine(SimonSequence sequence)
+    {
+        foreach (SimonInput input in sequence.Inputs)
+        {
+            switch (input.Direction)
+            {
+                case SimonInputDirection.UP:
+                    InputManager.Instance.SwipeStart(new Vector2(0, -2));
+                    InputManager.Instance.Swipe(new Vector2(0, 2));
+                    break;
+                case SimonInputDirection.DOWN:
+                    InputManager.Instance.SwipeStart(new Vector2(0, 2));
+                    InputManager.Instance.Swipe(new Vector2(0, -2));
+                    break;
+                case SimonInputDirection.RIGHT:
+                    InputManager.Instance.SwipeStart(new Vector2(-2, 1));
+                    InputManager.Instance.Swipe(new Vector2(2, 1));
+                    break;
+                case SimonInputDirection.LEFT:
+                    InputManager.Instance.SwipeStart(new Vector2(2, 1));
+                    InputManager.Instance.Swipe(new Vector2(-2, 1));
+                    break;
+            }
+            yield return new WaitForSeconds(0.3f); // wait for 100 milliseconds
+        }
     }
 }
