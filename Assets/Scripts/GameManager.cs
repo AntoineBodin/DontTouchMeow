a0f34e4945e18587820c60f1b49c30a560ecd799
Antoine Bodin <antoine-bodin@hotmail.fr>
2025-01-18 16:57:52 +0100

    feat: Add simons sequence generation following game state
    Made the method to generate the sequence static
    Add a method in GameManager to generate next sequence
    
    Refs: #24

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Simons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,30 @@ namespace Assets.Scripts
         public void StartGame()
         {
 
+        }
+
+        public SimonSequence GenerateNextSimonSequence()
+        {
+            int inputCount = 0;
+            float inputDuration = 1.2f;
+
+            switch (Status.CurrentState)
+            {
+                case MeowState.CALM:
+                    inputCount = 2 + Status.Step;
+                    inputDuration = 1.2f;
+                    break;
+                case MeowState.ANGRY:
+                    inputCount = 8;
+                    inputDuration = 1.2f - 0.05f * Status.Step;
+                    break;
+                case MeowState.XTRA_ANGRY:
+                    inputCount = 8 + Status.Step;
+                    inputDuration = 0.8f - 0.07f * Status.Step;
+                    break;
+            }
+
+            return SimonsManager.GenerateSequence(inputCount, inputDuration);
         }
 
         public void CalmMeow()
