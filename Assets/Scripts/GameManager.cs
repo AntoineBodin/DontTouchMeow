dfc97265e49b00452cd97a79519cbc6226c13b35
Antoine Bodin <antoine-bodin@hotmail.fr>
2025-01-18 15:10:27 +0100

    feat: Add HP management
    Add Player class holding the health
    Add Heal and TakeDamage methods
    
    Add GameManager class
    Add GameOver method
    
    Refs: #12

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        public void Start()
+        {
+            Instance = this;
+            Console.WriteLine("Game started!");
+        }
+
+        internal void GameOver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    internal class Player
+    {
+        private int health;
+        public Player(int health)
+        {
+            this.health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+               GameManager.Instance.GameOver();
+            }
+        }
+
+        public void Heal(int amount)
+        {
+            health += amount;
+        }
+
+    }
+}
